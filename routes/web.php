6747a7f44426a0ba298e9b2bcd1c69137e5559ae
Degbaun James <degbaunjames@gmail.com>
2025-05-29 17:36:42 +0100

    Ethic group routes guided

--- routes/web.php
+++ routes/web.php
@@ -13,19 +13,20 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-//Ethics Group
-Route::get('/igbo', [EthicsController::class, 'igbo'])->name('igbo');
-Route::get('/igbo/{id}', [EthicsController::class, 'igboShow'])->name('igbo.show');
-Route::get('/yoruba', [EthicsController::class, 'yoruba'])->name('yoruba');
-Route::get('/yoruba/{id}', [EthicsController::class, 'yorubaShow'])->name('yoruba.show');
-Route::get('/hausa', [EthicsController::class, 'hausa'])->name('hausa');
-Route::get('/hausa/{id}', [EthicsController::class, 'hausaShow'])->name('hausa.show');
+
 
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    //Ethics Group
+    Route::get('/igbo', [EthicsController::class, 'igbo'])->name('igbo');
+    Route::get('/igbo/{id}', [EthicsController::class, 'igboShow'])->name('igbo.show');
+    Route::get('/yoruba', [EthicsController::class, 'yoruba'])->name('yoruba');
+    Route::get('/yoruba/{id}', [EthicsController::class, 'yorubaShow'])->name('yoruba.show');
+    Route::get('/hausa', [EthicsController::class, 'hausa'])->name('hausa');
+    Route::get('/hausa/{id}', [EthicsController::class, 'hausaShow'])->name('hausa.show');
 });
 
 
